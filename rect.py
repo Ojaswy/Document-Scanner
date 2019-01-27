4e442c32e8114c400571857f4bbd907e80abb985
Ojaswy Akella <32218681+Ojaswy@users.noreply.github.com>
2019-01-27 22:20:01 +0530

    Update rect.py

--- rect.py
+++ rect.py
@@ -3,7 +3,7 @@ import numpy as np
 def rectify(h):
     h = h.reshape((4,2))
     hnew = np.zeros((4,2),dtype = np.float32)
- 
+  
     add = h.sum(1)
     hnew[0] = h[np.argmin(add)]
     hnew[2] = h[np.argmax(add)]
